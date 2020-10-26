3773b07ad1b2cc1d3fd125d13335d8e7b508a8df
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-10-27 00:16:50 +0200

    Add first passing test
    When the class is instantiated with an argument it respond to #read_file

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -1,2 +1,13 @@
 require 'simplecov'
+require 'log_parser'
 SimpleCov.start
+
+describe LogParser do
+  describe '#initialize' do
+    it 'initializes when log is given as an argument' do
+      file_path = 'log'
+      log_parser = LogParser.new(file_path)
+      expect(log_parser).to respond_to :read_file 
+    end
+  end
+end
