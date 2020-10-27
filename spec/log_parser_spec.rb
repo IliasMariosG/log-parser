5a1fbc5fceff9b1526a15ba6f85562da27a1ef15
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-10-27 13:59:06 +0200

    Add passing test for reading file with 8 rows in csv format

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -13,10 +13,12 @@ RSpec.describe LogParser do
     end
   end
   describe '#read_file' do
-    file_path = "data/webserver_sample_2_pages.log"
+    file_path = "data/webserver_sample.log"
     log_parser = LogParser.new(file_path)
-    it 'returns one array of two rows (2 subarrays) when it reads the file in CSV format' do
-      expected_output = [['/help_page/1 126.318.035.038'], ['/contact 184.123.665.067']]
+    it 'returns one array of 8 rows (8 subarrays) when it reads the file in CSV format' do
+      expected_output = [['/help_page/1 126.318.035.038'], ['/contact 184.123.665.067'], ['/home 184.123.665.067'],\
+                         ['/about/2 444.701.448.104'], ['/help_page/1 929.398.951.889'], ['/index 444.701.448.104'],\
+                         ['/help_page/1 722.247.931.582'], ['/about 061.945.150.735'], ['/help_page/1 646.865.545.408']]
       expect(log_parser.read_file).to eq(expected_output)
     end
   end
