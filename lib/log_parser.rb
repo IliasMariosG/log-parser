4150f93a4e593f369402f6d8dc411d42b57e7cf0
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-10-27 13:15:42 +0200

    Add passing test for reading file in csv format

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+require 'csv'
 
 # LogParsr class
 class LogParser
@@ -7,11 +8,12 @@ class LogParser
     @file_path = file_path
   end
 
+  # It reads the file and returns an array of rows
   def read_file
-   p  @file_path
+    CSV.read(@file_path)
   end
 end
 
 LogParser.new(ARGV[0]).read_file if $PROGRAM_NAME == __FILE__ # if this is the main file being used...
 
-#LogParser.new('webserver_sample_copy.log').read_file
+# LogParser.new('data/webserver_sample_1_page.log').read_file
--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -4,12 +4,20 @@ require 'simplecov'
 require 'log_parser'
 SimpleCov.start
 
-describe LogParser do
+RSpec.describe LogParser do
   describe '#initialize' do
     it 'initializes when log is given as an argument' do
       file_path = 'log'
       log_parser = LogParser.new(file_path)
       expect(log_parser).to respond_to :read_file
+    end
+  end
+  describe '#read_file' do
+    file_path = "data/webserver_sample_1_page.log"
+    log_parser = LogParser.new(file_path)
+    it 'returns one array of one row when it reads the file in CSV format' do
+      expected_output = [['/help_page/1 126.318.035.038']]
+      expect(log_parser.read_file).to eq(expected_output)
     end
   end
 end
