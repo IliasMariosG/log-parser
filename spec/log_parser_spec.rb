44fb2709676192ef197ef0e00c221dacd17129d2
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-10-28 18:10:02 +0200

    Fix expected_output

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe LogParser do
     it 'returns one array of 8 rows (8 subarrays) when it reads the file in CSV format' do
       expected_output = [['/help_page/1 126.318.035.038'], ['/contact 184.123.665.067'], ['/home 184.123.665.067'],\
                          ['/about/2 444.701.448.104'], ['/help_page/1 929.398.951.889'], ['/index 444.701.448.104'],\
-                         ['/help_page/1 722.247.931.582'], ['/about 061.945.150.735'], ['/help_page/1 646.865.545.408']]
+                         ['/help_page/1 722.247.931.582'], ['/about 061.945.150.735']]
       expect(log_parser.read_file).to eq(expected_output)
     end
   end
@@ -27,7 +27,7 @@ RSpec.describe LogParser do
     log_parser = LogParser.new(file_path)
     it 'returns a hash, each value being an array for each row in ascending order' do
       expected_output = { '/help_page/1' =>
-                          ['126.318.035.038', '929.398.951.889', '722.247.931.582', '646.865.545.408'],\
+                          ['126.318.035.038', '929.398.951.889', '722.247.931.582'],\
                           '/contact' => ['184.123.665.067'], '/home' => ['184.123.665.067'],\
                           '/about/2' => ['444.701.448.104'],\
                           '/index' => ['444.701.448.104'], '/about' => ['061.945.150.735'] }
@@ -38,34 +38,22 @@ RSpec.describe LogParser do
     file_path = 'data/webserver_sample.log'
     log_parser = LogParser.new(file_path)
     it 'returns an  array in descending order for all domains' do
-      input = { '/help_page/1' => ['126.318.035.038', '929.398.951.889', '722.247.931.582', '646.865.545.408'],\
-                '/contact' => ['184.123.665.067'],
-                '/home' => ['184.123.665.067'],
-                '/about/2' => ['444.701.448.104'],\
-                '/index' => ['444.701.448.104'],
-                '/about' => ['061.945.150.735'] }
-      expected_output = [['/help_page/1', 4],\
+      expected_output = [['/help_page/1', 3],\
                          ['/about', 1],\
                          ['/index', 1],\
                          ['/about/2', 1],\
                          ['/home', 1],\
                          ['/contact', 1]]
-      expect(log_parser.descending_order(input)).to eq(expected_output)
+      expect(log_parser.descending_order).to eq(expected_output)
     end
   end
   describe '#format_message' do
     file_path = 'data/webserver_sample.log'
     log_parser = LogParser.new(file_path)
     it 'returns a formatted message' do
-      input = [['/help_page/1', 4],\
-               ['/about', 1],\
-               ['/index', 1],\
-               ['/about/2', 1],\
-               ['/home', 1],\
-               ['/contact', 1]]
-      expected_output = '/help_page/1 4 visits, /about 1 visit, /index 1 visit, /about/2 1 visit, /home 1 visit, '\
+      expected_output = '/help_page/1 3 visits, /about 1 visit, /index 1 visit, /about/2 1 visit, /home 1 visit, '\
                         '/contact 1 visit'
-      expect(log_parser.format_message(input)).to eq(expected_output)
+      expect(log_parser.format_message).to eq(expected_output)
     end
   end
 end
