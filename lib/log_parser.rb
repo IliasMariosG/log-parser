b7826c5bafa5f38016e9ff5c3bed0512fd2df161
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-11-09 15:20:10 +0200

    Remove redundant assignment collection

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -18,13 +18,12 @@ class LogParser
 
   def parse_most_visits
     file_read = read_file
-    collection = file_read.each_with_object({}) do |row, hash_collection|
+    file_read.each_with_object({}) do |row, hash_collection|
       page_ip = row.first # e.g. first row "/help_page/1 126.318.035.038"
       page = page_ip.split(' ').first # e.g. "/help_page/1"
       ip = page_ip.split(' ').last
       hash_collection.key?(page) ? hash_collection[page] << ip : hash_collection[page] = [ip]
     end
-    collection # hash
   end
 
   def descending_order
